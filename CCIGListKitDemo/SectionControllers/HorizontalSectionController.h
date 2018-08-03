120d436592cecbc8a7847d7ac291e1bdb549c68e
CC <ccworld1000@gmail.com>
2018-08-03 12:50:26 +0800

    CCAutoTag : 20180803125026

---
+++ CCIGListKitDemo/SectionControllers/HorizontalSectionController.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalSectionController.h
+//  CCIGListKitDemo
+//
+//  Created by dengyouhua on 2018/8/3.
+//  Copyright © 2018 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import "IGListSectionController.h"
+
+@interface HorizontalSectionController : IGListSectionController
+
+@end
---
+++ CCIGListKitDemo/SectionControllers/HorizontalSectionController.m
@@ -0,0 +1,13 @@
+//
+//  HorizontalSectionController.m
+//  CCIGListKitDemo
+//
+//  Created by dengyouhua on 2018/8/3.
+//  Copyright © 2018 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import "HorizontalSectionController.h"
+
+@implementation HorizontalSectionController
+
+@end
