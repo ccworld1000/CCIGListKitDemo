14f4d62e933f48cb8fe48c795af22dcc68c64c71
CC <ccworld1000@gmail.com>
2018-08-27 18:04:04 +0800

    CCAutoTag : 20180827180404

---
+++ CCIGListKitDemo/ViewControllers/StackedViewController.h
@@ -0,0 +1,13 @@
+//
+//  StackedViewController.h
+//  CCIGListKitDemo
+//
+//  Created by dengyouhua on 2018/8/27.
+//  Copyright © 2018 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StackedViewController : UIViewController
+
+@end
---
+++ CCIGListKitDemo/ViewControllers/StackedViewController.m
@@ -0,0 +1,88 @@
+//
+//  StackedViewController.m
+//  CCIGListKitDemo
+//
+//  Created by dengyouhua on 2018/8/27.
+//  Copyright © 2018 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import "StackedViewController.h"
+#import "WorkingRangeSectionController.h"
+#import "DisplaySectionController.h"
+#import "HorizontalSectionController.h"
+
+@interface StackedViewController () <IGListAdapterDataSource>
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) IGListAdapter *adapter;
+
+@property (nonatomic, strong) NSArray *data;
+
+@end
+
+@implementation StackedViewController
+
+- (UICollectionView *)collectionView {
+    if (!_collectionView) {
+        _collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[UICollectionViewFlowLayout new]];
+        _collectionView.backgroundColor = [[CCColor whiteColor] colorWithAlphaComponent:0.95];
+    }
+    
+    return _collectionView;
+}
+
+- (IGListAdapter *)adapter {
+    if (!_adapter) {
+        _adapter = [[IGListAdapter alloc] initWithUpdater:[IGListAdapterUpdater new] viewController:self workingRangeSize:1];
+    }
+    
+    return _adapter;
+}
+
+- (NSArray *)data {
+    if (!_data) {
+        _data = @[
+                  @(128),
+                  @(256),
+                  @(64),
+                  ];
+    }
+    
+    return _data;
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    self.collectionView.frame = self.view.bounds;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self.view addSubview:self.collectionView];
+    self.adapter.collectionView = self.collectionView;
+    self.adapter.dataSource = self;
+}
+
+- (NSArray<id <IGListDiffable>> *)objectsForListAdapter:(IGListAdapter *)listAdapter {
+    return self.data;
+}
+
+- (IGListSectionController *)listAdapter:(IGListAdapter *)listAdapter sectionControllerForObject:(id)object {
+    IGListStackedSectionController *sectionController = [[IGListStackedSectionController alloc] initWithSectionControllers:@[
+                                                                                                                             [WorkingRangeSectionController new],
+                                                                                                                             [DisplaySectionController new],
+                                                                                                                             [HorizontalSectionController new],
+                                                                                                                             ]];
+    sectionController.inset = UIEdgeInsetsMake(0, 0, 20, 0);
+    return sectionController;
+}
+
+- (nullable UIView *)emptyViewForListAdapter:(IGListAdapter *)listAdapter {
+    return nil;
+}
+
+
+@end
